f756f43650e630ba7866d3b094ac2ab5cfabf400
Anton Zinchenko <d-krabike@yandex.ru>
2019-11-15 15:44:58 +0500

    Anton Zinchenko rework 0.0.1

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,149 +1,129 @@
 "use strict";
 
-// получить элемент, имеющий id="myDiv"
-var myDiv = document.getElementById("myDiv"); // подпишемся на событие click этого элемента
-
 window.onload = function() {
-  // создадим объект XMLHttpRequest
-  var xhr = new XMLHttpRequest(); //настраиваем запрос: GET - метод, data.txt - URL-адрес по которому будет посылаться запрос, false - синхронный запрос
 
-  xhr.open('GET', './api/products.js', false); // отправляем данные на сервер с помощью метода send
+  var products = API.products;
+  products.forEach(function(product) {
 
-  xhr.send(); // если статус ответа 200 (OK) то
+    // Создаем контейнер для корзины
 
-  if (xhr.status == 200) {
-    // выведем в элемент, имеющий id="answer", ответ сервера
-    var products = API.products;
-    products.forEach(function(product) {
+    var divContainer = main.appendChild(document.createElement('div'));
 
-      // Корзина
+    divContainer.id = product.id;
+    divContainer.classList.add('container');
 
-      var div = main.appendChild(document.createElement('div'));
+    // Создаем контейнер для картинки
 
-      div.id = product.id;
-      div.classList.add('container');
+    var divImg = divContainer.appendChild(document.createElement('div'));
+    var img = divImg.appendChild(document.createElement('img'));
 
-      // Картинка
+    divImg.id = product.id;
+    img.src = product.img;
+    divImg.classList.add('product-img');
 
-      var divImg = div.appendChild(document.createElement('div'));
+    // Закидываем контент в контейнер
 
-      divImg.id = product.id;
+    var divContent = divContainer.appendChild(document.createElement('div'));
+    var contentTitle = divContent.appendChild(document.createElement('div'));
+    var contentPrice = divContent.appendChild(document.createElement('div'));
 
-      var img = divImg.appendChild(document.createElement('img'));
+    divContent.classList.add('product-content');
+    divContent.id = product.id;
 
-      img.src = product.img;
+    contentTitle.innerHTML = product.title;
+    contentTitle.classList.add('content-title');
 
-      divImg.classList.add('product-img');
+    contentPrice.innerHTML = "".concat(product.price.toLocaleString('ru-RU'), " \u0440\u0443\u0431.");
+    contentPrice.classList.add('content-price');
 
-      // Контент
+    // Закидываем кнопки в контейнер
 
-      var divContent = div.appendChild(document.createElement('div'));
+    var divAction = divContainer.appendChild(document.createElement('div'));
+    var wrapperBtnOnShopingList = divAction.appendChild(document.createElement('div'));
+    var btnOnShopingList = wrapperBtnOnShopingList.appendChild(document.createElement('button'));
+    var wrapperBtnOnCart = divAction.appendChild(document.createElement('div'));
+    var btnOnCart = wrapperBtnOnCart.appendChild(document.createElement('button'));
 
-      divContent.classList.add('product-content');
-      divContent.id = product.id;
+    divAction.classList.add('product-action');
+    wrapperBtnOnShopingList.classList.add('wrapper-BtnOnShopingList');
+    btnOnShopingList.id = product.id;
+    wrapperBtnOnCart.classList.add('wrapper-BtnOnCart');
+    btnOnCart.id = product.id;
 
-      var contentTitle = divContent.appendChild(document.createElement('div'));
+    btnOnShopingList.innerHTML = "Заказать";
+    btnOnCart.innerHTML = "В корзину";
 
-      contentTitle.innerHTML = product.title;
-      contentTitle.classList.add('content-title');
-
-      var contentPrice = divContent.appendChild(document.createElement('div'));
-
-      contentPrice.innerHTML = "".concat(product.price.toLocaleString('ru-RU'), " \u0440\u0443\u0431.");
-      contentPrice.classList.add('content-price');
-
-      // Кнопки
-
-      var divAction = div.appendChild(document.createElement('div'));
-
-      divAction.classList.add('product-action');
-
-      var btn1 = divAction.appendChild(document.createElement('button'));
-
-      btn1.id = product.id;
-
-      var btn2 = divAction.appendChild(document.createElement('button'));
-
-      btn2.id = product.id;
-      btn1.innerHTML = "Заказать";
-      btn2.innerHTML = "В корзину";
-      btn1.classList.add('on-order');
-      btn2.classList.add('on-shoping-list');
-    });
-  }
-
-  // /Корзина
+    btnOnShopingList.classList.add('on-order');
+    btnOnCart.classList.add('on-shoping-list');
 
 
-  var btn1 = document.querySelectorAll(".on-shoping-list");
-  var shopingCart = document.querySelector(".shopping-cart");
-  var productImg = document.querySelectorAll(".product-img");
-  var productContent = document.querySelectorAll(".product-content");
+    var btnOnShopingList = document.querySelectorAll(".on-shoping-list");
+    var shopingCart = document.querySelector(".shopping-cart");
+    var productImg = document.querySelectorAll(".product-img");
+    var productContent = document.querySelectorAll(".product-content");
 
-  //Обработчик кнопки "В корзину"
+    // Обработчик отправки в корзину
 
-  [].forEach.call(btn1, function(el) {
-    el.onclick = function(e) {
+    wrapperBtnOnCart.addEventListener('click', function(e) {
+      if (!e.target.classList.contains('on-shoping-list')) {
+        return;
+      };
       shopingCart.style.display = '';
 
       var cartInner = shopingCart.appendChild(document.createElement('div'));
+      var innerImg = cartInner.appendChild(document.createElement('div'));
+      var id = e.path[3].id;
+      var cloneImg = productImg[id - 1].cloneNode(true);
+      var cloneContent = productContent[id - 1].cloneNode(true);
+      var itemDelete = innerImg.appendChild(document.createElement('div'));
+
 
       cartInner.classList.add('cart-container');
-
-      var innerImg = cartInner.appendChild(document.createElement('div'));
-
       innerImg.classList.add('inner-cart');
-      innerImg.id = "".concat([el.id - 1], "Cart"); // innerImg = test[el.id - 1].cloneNode(true);
-
-      var cloneImg = productImg[el.id - 1].cloneNode(true);
+      innerImg.id = "".concat([e.path[3].id], "Cart");
 
       innerImg.appendChild(cloneImg);
       cloneImg.classList.add('cart-img');
 
-      var cloneContent = productContent[el.id - 1].cloneNode(true);
-
       innerImg.appendChild(cloneContent);
       cloneContent.classList.add('cart-Content');
 
-      var itemDelete = innerImg.appendChild(document.createElement('div'));
-
       itemDelete.classList.add('cart-toggle');
-      itemDelete.id = "".concat([el.id - 1], "Toggle");
+      itemDelete.id = "".concat([id - 1], "Toggle");
 
+      // Обработчик закрытия корзины
       var closeCartBtn = document.querySelector(".cart-toggle");
-      var cartContainer = document.querySelectorAll(".cart-container");
-
-      // Обработчки кнопки удаления из корзины
-
       closeCartBtn.onclick = function() {
         shopingCart.style.display = 'none';
         document.querySelector(".cart-container").remove();
       };
-    };
-  });
+    });
 
-  // Обработчик кнопки "Заказ"
+    // Обработчик кнопки "Заказ"
 
-  var btn2 = document.querySelectorAll(".on-order");
-  var orderList = document.querySelector(".order-list");
-  var productImg = document.querySelectorAll(".product-img");
-  var productTitle = document.querySelectorAll(".content-title");
-  var orderTitle = document.querySelector(".order-title");
-  var orderContent = document.querySelector(".order-content");
-  var orderPrice = document.querySelectorAll(".content-price");
+    var btnOnCart = document.querySelectorAll(".on-order");
+    var orderList = document.querySelector(".order-list");
+    var productImg = document.querySelectorAll(".product-img");
+    var productTitle = document.querySelectorAll(".content-title");
+    var orderTitle = document.querySelector(".order-title");
+    var orderContent = document.querySelector(".order-content");
+    var orderPrice = document.querySelectorAll(".content-price");
 
-  [].forEach.call(btn2, function(el) {
-    el.onclick = function(e) {
+    // Обработчик отправки в заказ
+
+    wrapperBtnOnShopingList.addEventListener('click', function(e) {
+      if (!e.target.classList.contains('on-order')) {
+        return;
+      };
       orderList.style.display = '';
+      var id = e.path[3].id;
+      var orderImgBlock = orderContent.innerHTML = "".concat(productImg[id - 1].innerHTML, " </br>    ").concat(orderPrice[id - 1].innerHTML);
+      orderTitle.innerHTML = productTitle[id - 1].innerHTML;
 
-      orderTitle.innerHTML = productTitle[el.id - 1].innerHTML;
-
-      var orderImgBlock = orderContent.innerHTML = "".concat(productImg[el.id - 1].innerHTML, " </br>    ").concat(orderPrice[el.id - 1].innerHTML);
-    };
+      var closeOrderBtn = document.querySelector(".order-close");
+      closeOrderBtn.onclick = function() {
+        orderList.style.display = 'none';
+      };
+    });
   });
-  var closeOrderBtn = document.querySelector(".order-close");
-
-  closeOrderBtn.onclick = function() {
-    orderList.style.display = 'none';
-  };
 };
