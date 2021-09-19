91c70f38a1a9ff72aa1ac4db2ae833027872a780
Sharick <aleksandremelanov305@gmail.com>
2021-09-19 15:29:29 +0300

    тестовое задание емельянов

--- src/api/products.js
+++ src/api/products.js
@@ -71,3 +71,136 @@ var API = {
     },           
   ]
 };
+/*   Отрисовка товаров  */
+
+let products = API.products,
+productsContainer = document.querySelector('.products__container');
+
+products.forEach(function(item){
+
+  productsContainer.insertAdjacentHTML("afterbegin", `
+  <div class="products__item product-item" id="${item.id}">
+    <div class="product-item__img-wrapper">
+        <img src="${item.img}" alt="" loading="lazy"ц>
+    </div>
+    <div class="product-item__info">
+        <div  class="product-item__name">${item.title}</div>
+        <div class="product-item__price">
+          <span class="product-item__price-value">${item.price.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1 ')}</span>
+          <span class="product-item__price-currency">руб.</span> 
+        </div>
+    </div>
+    <div class="product-item__actions">
+        <button class="product-item__order-button button button_cerise">Заказать</button>
+        <button class="product-item__cart-button button button_gray">В корзину</button>
+    </div>
+</div>
+  `);
+
+});
+
+/*   Попап  */
+
+if (typeof popUp === 'undefined') {
+  var popUp = []
+}
+popUp = document.querySelectorAll('.pop-up');
+
+function popOpen(popupType,customId, itemId) {
+
+  if(popupType == "toOrder" ){
+
+  let siteBody = document.querySelector('body');
+  siteBody.classList.add('stop-scroll');
+  }
+
+  for (let i = 0; i < popUp.length; i++) {
+      if (popUp[i].id == customId) {
+
+          popUp[i].classList.add('pop-up_active');
+
+          if(popupType == "toOrder" ){
+
+            let popapContn =popUp[i].querySelector(".pop-up__content");
+            let product = products.find(item => item.id == itemId);
+
+            popapContn.querySelector(".pop-up__product-name").innerHTML = product.title;
+            popapContn.querySelector(".pop-up__product-img").setAttribute("src",product.img);
+            popapContn.querySelector(".pop-up__product-price-value").innerHTML = product.price.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1 ');
+
+          } else if(popupType == "toCart" ){
+
+            let cartList =popUp[i].querySelector(".cart-pop-up__list");
+            let product = products.find(item => item.id == itemId);
+            cartList.insertAdjacentHTML("afterBegin", `
+            <li class="cart-pop-up__item cart-item">
+              <img src="${product.img}" alt="" class="cart-item__img">
+              <div class="cart-item__text-cont">
+                  <div class="cart-item__name">${ product.title}</div>
+                  <div class="cart-item__prise">
+                      <span class="cart-item__prise-value">${product.price.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1 ')}</span>
+                      <span class="cart-item__prise-currency">руб.</span>
+                  </div>
+              </div>
+              <button class="cart-pop-up__remove-button">
+                <svg class="svg">
+                    <use xlink:href="#close"></use>
+                </svg>
+              </button>
+          </li>`);
+          }
+     
+      }
+  }
+
+}
+
+function popClose(e) {
+  let siteBody = document.querySelector('body');
+  siteBody.classList.remove('stop-scroll');
+  if(e.target.closest(".pop-up")){
+    e.target.closest(".pop-up").classList.remove('pop-up_active');
+  }
+
+}
+
+/*   Переход к попапу с заказом  */
+
+let orderButtons = document.getElementsByClassName("product-item__order-button");
+
+Array.prototype.forEach.call(orderButtons, function(item) {
+  item.addEventListener("click", function(){
+    popOpen("toOrder","js-customer-pop-form_order", item.closest(".product-item").getAttribute("id"))
+
+    
+  })
+});
+/*   Добавление товара в корзину  */
+
+let cartButtons = document.getElementsByClassName("product-item__cart-button");
+
+Array.prototype.forEach.call(cartButtons, function(item) {
+  item.addEventListener("click", function(){
+    popOpen("toCart", "js-customer-pop-form_cart", item.closest(".product-item").getAttribute("id"))
+  });
+
+});
+
+/*   Удаление  товара из корзины  */
+
+let cartPopUp = document.querySelector(".pop-up_cart");
+cartPopUp.addEventListener("click", function(e){
+  if (e.target.closest(".cart-pop-up__remove-button")){
+   let cart =  e.target.closest(".cart-item");
+    cart.parentNode.removeChild(cart);
+  }
+
+  /*   Если в корзине не осталось товаров - скрыть корзину  */
+
+  if (document.querySelectorAll(".cart-pop-up__list li").length <1){
+
+    document.querySelector(".pop-up_cart").classList.remove('pop-up_active');
+
+  }
+
+})
