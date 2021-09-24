d9e978a00884d6f7574c604a02b48c9fc51efa87
Andrey Kloninger <veto.aion@gmail.com>
2021-09-24 11:43:37 +0500

    [add]: added styles and event handlers for buttons

--- src/components/Product.js
+++ src/components/Product.js
@@ -15,15 +15,41 @@ class Product {
     return cardElement;
   }
 
+  _priceConversion() {
+    let newPrice = String(this._price);
+    if (newPrice.length === 4) {
+      newPrice = `${newPrice.slice(0, 1)} ${newPrice.slice(1)}`;
+    }
+    if (newPrice.length === 5) {
+      newPrice = `${newPrice.slice(0, 2)} ${newPrice.slice(2)}`;
+    }
+    if (newPrice.length === 6) {
+      newPrice = `${newPrice.slice(0, 3)} ${newPrice.slice(3)}`;
+    }
+    return `${newPrice} руб.`;
+  }
+
   generateProduct() {
     this._element = this._getTemplate();
     const productImage = this._element.querySelector(".product__image");
     const productName = this._element.querySelector(".product__title");
     const productPrice = this._element.querySelector(".product__price");
+    const buttonOrder = this._element.querySelector(".product__button_order");
+    const buttonCart = this._element.querySelector(".product__button_cart");
     productImage.src = this._urlImg;
     productImage.alt = this._title;
     productName.textContent = this._title;
-    productPrice.textContent = this._price;
+    productPrice.textContent = this._priceConversion();
+    this._setEventListener(buttonOrder, buttonCart);
     return this._element;
+  }
+
+  _setEventListener(btnOrder, btnCart) {
+    btnOrder.addEventListener("click", () => {
+      console.log("Оформить заказ!");
+    });
+    btnCart.addEventListener("click", () => {
+      console.log("Товар добавлен!");
+    });
   }
 }
